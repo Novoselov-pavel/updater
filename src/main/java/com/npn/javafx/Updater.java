8c0d385f4911e72a5adf38328772dc54156e0164
Novoselov-pavel <Novosselov.pavel@gmail.com>
2020-03-24 23:23:16 +0300

    Initial commit

---
+++ src/main/java/com/npn/javafx/Updater.java
@@ -0,0 +1,16 @@
+package com.npn.javafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Updater extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
